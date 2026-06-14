8b498d98b487f34f3fccf9ffb57172a1ea3154b1
Nexora studios <nexorastudiosss@gmail.com>
2026-06-14 21:56:44 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// SCROLL REVEAL ANIMATION
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+const hiddenElements = document.querySelectorAll(
+  ".feature-card, .portfolio-card, .testimonial-card, .pricing-card, .cta"
+);
+
+hiddenElements.forEach((el) => observer.observe(el));
